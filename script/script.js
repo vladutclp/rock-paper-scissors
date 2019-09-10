405eb769667cb7511ac12e7ae4eccb591e4e753a
Claps <vladut.clapou@gmail.com>
2019-09-10 20:38:37 +0300

    First graphical version

--- script/script.js
+++ script/script.js
@@ -1,9 +1,21 @@
-const computerChoice = ['rock', 'paper', 'scissors'];
-var yourScore = 0;
-var computerScore = 0;
+const computerChoice = ['rock', 'paper', 'scissors']; // Game choices
+let yourScore = 0, computerScore = 0; // User score and computer score
+let nrGame = 0;
+
+
+const scoreDiv = document.querySelector(".tabela");
+const yourScoreDisplayed = document.querySelector('#yourScore'), computerScoreDisplayed = document.querySelector('#computerScore'); //Paragraphs to display score
+yourScoreDisplayed.textContent = "Your score: " + yourScore; // Display user score on the page
+computerScoreDisplayed.textContent = "Computer score: " + computerScore;	// Display computer score on the page
+
+
+
+scoreDiv.appendChild(yourScoreDisplayed);
+scoreDiv.appendChild(computerScoreDisplayed);
 
 function computerPlay(){
 	return computerChoice[Math.floor(Math.random() * computerChoice.length)];
+	
 }
 
 
@@ -14,14 +26,15 @@ function playRound(playerSelection, computerSelection) {
 	if(playerSelection == 'rock'){
 		if(computerSelection == 'paper'){
 			computerScore++;	
+			computerScoreDisplayed.textContent = "Computer score: " + computerScore;
 		}
 		else{
 			if(computerSelection == 'scissors'){
-				yourScore++;	
+				yourScore++;
+				yourScoreDisplayed.textContent = "Your score: " + yourScore;	
 			}
 			else{
-				yourScore++;
-				computerScore++;
+				//
 			}
 		}
 	}
@@ -29,66 +42,163 @@ function playRound(playerSelection, computerSelection) {
 		if(playerSelection == 'paper'){
 			if(computerSelection == 'scissors'){
 				computerScore++;
+				computerScoreDisplayed.textContent = "Computer score: " + computerScore;
 			}
 			else{
 				if(computerSelection == 'rock'){
-					yourScore++;	
+					yourScore++;
+					yourScoreDisplayed.textContent = "Your score: " + yourScore;	
 				}
 				else{
-					yourScore++;
-					computerScore++;	
+					//
 				}
 			}
 		}
 		else{
 			if(computerSelection == 'rock'){
-				computerScore++;	
+				computerScore++;
+				computerScoreDisplayed.textContent = "Computer score: " + computerScore;	
 			}
 			else{
 				if(computerSelection == 'paper'){
-					yourScore++;	
+					yourScore++;
+					yourScoreDisplayed.textContent = "Your score: " + yourScore;	
 				}
 				else{
-					yourScore++;
-					computerScore++;	
+					//
 				}
 			}
 		}
 
-	
+		nrGame++;
+}
 
+
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+
+
+
+
+
+
+const reset = document.querySelector("#reset");
+reset.addEventListener('click', resetScore);
+
+function printScore(yourScore, computerScore){
+	yourScoreDisplayed.textContent = "Your score: " + yourScore;
+	computerScoreDisplayed.textContent = "Computer score: " + computerScore;
+}
+
+function printChoice(yourChoice, computerChoice){
 
 }
 
 function resetScore(){
 	yourScore = 0;
 	computerScore = 0;
+
+	startGame.classList.remove("remove");
+	rock.disabled = false;
+	paper.disabled = false;
+	scissors.disabled = false;
+	finalMessage.textContent = "";
+	computerChoiceDisplayed.textContent = "";
+	yourChoiceDisplayed.textContent = "";
+	printScore(yourScore, computerScore);
+		
+	hideDiv.classList.add('remove');
+	}
+
+paper.addEventListener('click', playGame);
+scissors.addEventListener('click', playGame);
+rock.addEventListener('click', playGame);
+
+const finalMessage = document.querySelector('#finalMessage');
+var computerChoiceDisplayed = document.querySelector(".computerChoice");
+var yourChoiceDisplayed = document.querySelector(".yourChoice");
+//scoreDiv.appendChild(finalMessage);
+//scoreDiv.appendChild(computerChoiceDisplayed);
+//scoreDiv.appendChild(yourChoiceDisplayed);
+function playGame(){
+
+	var id = this.id;
+	var compChoice;
+	
+
+
+	switch(id){
+		case 'paper':
+			compChoice = computerPlay();
+			playRound('paper', compChoice);
+			console.log("Computer score: " + computerScore + "\nYour score: " + yourScore + "\nComputer played: " + compChoice + "\nYour choice: paper");
+			computerChoiceDisplayed.textContent = "Computer choose: " + compChoice;
+			yourChoiceDisplayed.textContent = "You choose: paper";
+			yourScoreDisplayed.textContent = "Your score: " + yourScore;
+			computerScoreDisplayed.textContent = "Computer score: " + computerScore;
+			break;
+
+		
+		case 'rock':
+			compChoice = computerPlay();
+			playRound('rock', compChoice);
+			console.log("Computer score: " + computerScore + "\nYour score: " + yourScore + "\nComputer played: " + compChoice + "\nYour choice: rock");
+			computerChoiceDisplayed.textContent = "Computer choose: " + compChoice;
+			yourChoiceDisplayed.textContent = "You choose: rock";
+			yourScoreDisplayed.textContent = "Your score: " + yourScore;
+			computerScoreDisplayed.textContent = "Computer score: " + computerScore;
+			break;
+		
+
+		case 'scissors':
+			compChoice = computerPlay();
+			playRound('scissors', compChoice);
+			console.log("Computer score: " + computerScore + "\nYour score: " + yourScore + "\nComputer played: " + compChoice + "\nYour choice: scissors");
+			computerChoiceDisplayed.textContent = "Computer choose: " + compChoice;
+			yourChoiceDisplayed.textContent = "You choose: scissors";
+			yourScoreDisplayed.textContent = "Your score: " + yourScore;
+			computerScoreDisplayed.textContent = "Computer score: " + computerScore;
+			break;
+		
+
+		default:
+			break;
+	}
+	
+	console.log(nrGame);
+
+	if(nrGame == 5){
+
+		//alert("gata");
+		nrGame = 0;
+		
+		if(yourScore > computerScore)
+			finalMessage.textContent = "You won!!!";
+		else if(yourScore < computerScore)
+			finalMessage.textContent = "You lost!"
+		else
+			finalMessage.textContent = "ITS A FUCKIN TIEEEEEEE!!";
+		
+		rock.disabled = true;
+		paper.disabled = true;
+		scissors.disabled = true;
+		
+	}
+	
 }
 
-function game(){
-	let playerInput ;
-	let computerSelection, nrGame = 0;
-	while(nrGame < 5){
-		playerInput = prompt("Choose your destiny");
-		computerSelection = computerPlay();
-		console.log("Computer choice: " + computerSelection + "\nYou choice: " + playerInput);
-		playRound(playerInput, computerSelection);
-		nrGame++;
-		console.log("Computer score: " + computerScore + "\nYour score: " + yourScore);
-	}
+const startGame = document.querySelector(".start");
 
-	if(yourScore > computerScore){
-		alert("You Won");
-	}
-	else if(yourScore < computerScore){
-		alert("You lost");
-	}
-	else{
-		alert("It's a tie!")
-	}
+const hideDiv = document.querySelector("#removeID");
 
-	resetScore();
+function removeClass(){
+	hideDiv.classList.remove("remove");
+	startGame.classList.add("remove");
 }
 
+startGame.addEventListener('click', removeClass);
 
-game();
+
+
+
